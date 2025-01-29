1a65e5c0a338e9bdd442173e3e2cd314fe3e0a32
Rra549 <96408732+Rra549@users.noreply.github.com>
2025-01-29 22:06:31 +0700

    Add files via upload

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2025 at 02:22 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Jan 08, 2025 at 07:01 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,35 +35,6 @@ CREATE TABLE `mahasiswa` (
   `nomor` varchar(15) NOT NULL,
   `jurusan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `mahasiswa`
---
-
-INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `email`, `nomor`, `jurusan`) VALUES
-(1, 'jasa', '8590285', 'fjskj@gmail.com', '85925', 'it'),
-(2, 'jasa', '8590285', 'fjskj@gmail.com', '85925', 'it'),
-(3, 'raki', '247854', 'fjskj@gmail.com', '85925', 'it');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
